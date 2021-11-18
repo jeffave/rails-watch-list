d9a53df7316810942efba6792f5201beacc2af1b
Jefferson Avelino <jefferson.alves@msn.com>
2021-11-18 11:57:04 -0300

    Ready to start.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,17 @@
 # Load the Rails application.
+const { environment } = require('@rails/webpacker')
 require_relative 'application'
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+module.exports = environment
 
 # Initialize the Rails application.
 Rails.application.initialize!
